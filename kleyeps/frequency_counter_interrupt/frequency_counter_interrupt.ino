9802d034fd94c34516ac1b61ea864f79664a864c
Jonas Reubelt <jonas.reujon.reubelt@fau.de>
2018-09-18 14:16:24 +0200

    adds interrupt interrupt

--- kleyeps/frequency_counter_interrupt/frequency_counter_interrupt.ino
+++ kleyeps/frequency_counter_interrupt/frequency_counter_interrupt.ino
@@ -1,7 +1,7 @@
 const byte interruptPin1 = 2;
 const byte interruptPin2 = 3;
-long int n1 = 0;
-long int n2 = 0;
+volatile long int n1 = 0;
+volatile long int n2 = 0;
 long int timestamp;
 long int now;
 long int diff;
@@ -17,16 +17,18 @@ void setup() {
 void loop() {
    delay(1000);
 
+   noInterrupts();
    now = micros();
-   diff = (now - timestamp) * 1000000;
-   
-   Serial.print((float)n1/diff);
+   diff = (now - timestamp) / 1000000;
+  
+   Serial.print(float(n1)/diff);
    Serial.print(" ");
-   Serial.println((float)n2/diff);
+   Serial.println(float(n2)/diff);
 
    n1 = 0;
    n2 = 0;
    timestamp = now;
+   interrupts();
 }
 
 void increase1() {
